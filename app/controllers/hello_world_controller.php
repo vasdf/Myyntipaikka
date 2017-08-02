3e364676e129aa96fdbdafd62569b671b98054e0
Valtteri <valtsu54@hotmail.com>
2017-08-02 22:46:43 +0300

    näkymiä lisätty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,22 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
+    }
+
+    public static function item_list(){
+      View::make('suunnitelmat/item_list.html');
+    }
+
+    public static function login(){
+      View::make('suunnitelmat/login.html');
+    }
+
+    public static function profile(){
+      View::make('suunnitelmat/profile.html');
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,19 @@
     HelloWorldController::index();
   });
 
+  $routes->get('/item_list', function() {
+  	HelloWorldController::item_list();
+  });
+
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+
+  $routes->get('/profile', function() {
+    HelloWorldController::profile();
+  });
+
+
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
